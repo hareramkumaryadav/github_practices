1d4b8ec4cf38a11d59bfc3dbd2e426e840f626dd
hareramkumaryadav <hareramkumaryadav26@gmail.com>
2026-05-24 19:52:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Hello, World!');
+// This is a simple JavaScript file that logs "Hello, World!" to the console. You can run this code in a JavaScript environment, such as a web browser's developer console or Node.js, to see the output.   Feel free to modify the message or add more functionality as needed! \
+// For example, you could create a function to greet a user by name:
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+greet('Alice');
+greet('Bob');
