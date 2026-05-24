3c17ad5cdc36bfd30aafff13f05729261126f4dd
hareramkumaryadav <hareramkumaryadav26@gmail.com>
2026-05-24 20:32:32 +0530

    faction adedd to mian branch

--- index.js
+++ index.js
@@ -23,3 +23,15 @@ function factorial(n) {
 }   
 console.log(`The factorial of 5 is: ${factorial(5)}`);
 
+function  fibonacci(n) {
+    if (n <= 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+console.log(`The 10th Fibonacci number is: ${fibonacci(10)}`);
+
+
